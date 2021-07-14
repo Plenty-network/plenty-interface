742d37722dcab00d8bd06a79731603732d8ed0dc
Abhishek Mishra <abhishek@tezsure.com>
2021-07-14 18:03:47 +0530

    farm page

--- src/Pages/Farms.js
+++ src/Pages/Farms.js
@@ -1,7 +1,80 @@
-import React from "react";
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 
-const Farms = () => {
-  return <div>Farms</div>;
+import FarmCard from '../Components/FarmCard/FarmCard';
+
+import plentyXtz from '../assets/images/farms/plenty-xtz.png';
+
+const Farms = (props) => {
+  const farmsList = [
+    {
+      image: plentyXtz,
+      multi: '100',
+      title: 'PLENTY / XTZ LP',
+      apr: 0,
+      apy: '2621',
+      earn: 'PLENTY',
+      fee: '0%',
+      earned: 0,
+      deposit: 'PLENTY - XTZ LP',
+      liquidity: '100000',
+      withdrawalFee: '0%',
+      balance: 0,
+      userBalance: 0,
+      URL: '',
+      active: true,
+      source: 'Quipuswap LP',
+      rewards: '1000 PLENTY / DAY',
+    },
+    {
+      image: plentyXtz,
+      multi: '100',
+      title: 'hDAO / PLENTY LP',
+      apr: 0,
+      apy: '2621',
+      earn: 'PLENTY',
+      fee: '0%',
+      earned: 0,
+      deposit: 'PLENTY - XTZ LP',
+      liquidity: '1000',
+      withdrawalFee: '0%',
+      balance: 0,
+      userBalance: 0,
+      URL: '',
+      active: true,
+      source: 'Plenty',
+      rewards: '1000 PLENTY / DAY',
+    },
+    {
+      image: plentyXtz,
+      multi: '100',
+      title: 'KALAM / PLENTY LP',
+      apr: 0,
+      apy: '2621',
+      earn: 'PLENTY',
+      fee: '0%',
+      earned: 0,
+      deposit: 'PLENTY - XTZ LP',
+      liquidity: '5000',
+      withdrawalFee: '0%',
+      balance: 0,
+      userBalance: 0,
+      URL: '',
+      active: true,
+      source: 'Plenty',
+      rewards: '1000 PLENTY / DAY',
+    },
+  ];
+  return (
+    <Container fluid className="page-layout-container">
+      <Row>
+        {farmsList.map((farm, index) => {
+          return <FarmCard key={index} {...farm} {...props} />;
+        })}
+      </Row>
+    </Container>
+  );
 };
 
 export default Farms;
