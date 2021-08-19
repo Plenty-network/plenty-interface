fcef2becf636f929d0b1f72d3f6aacd669c5dcc5
Vijay Pushkin <mail@vijaypushkin.me>
2021-08-19 21:26:56 +0530

    Dark theme added 80%

--- src/themes.js
+++ src/themes.js
@@ -2,6 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 import styled from 'styled-components';
 
 export const lightTheme = {
+  name: 'light',
   body: '#F9FAFA',
   p: '#4e5d78',
   span: '#4e5d78',
@@ -29,6 +30,7 @@ export const lightTheme = {
 
 
 export const darkTheme = {
+  name: 'dark',
   body: 'linear-gradient(108.61deg, #04152E -24.15%, #343C49 99.76%)',
   p: '#FFFFFF',
   span: '#FFFFFF',
@@ -51,6 +53,50 @@ export const darkTheme = {
   iconHoverBgStartColor: '#7028E4',
   iconHoverBgEndColor: '#E5B2CA',
 };
+
+const themeColorsLight = {
+  '--theme-primary-1': '#5C0FAE',
+  '--theme-primary-2': '#D0BDE6',
+  '--theme-secondary-1': '#E3D1FF',
+  '--theme-primary-gradient-1': '#7028E4',
+  '--theme-primary-gradient-2': '#E5B2CA',
+  '--theme-secondary-gradient-1': '#F5F0FD',
+  '--theme-secondary-gradient-2': '#FDFAFB',
+  '--theme-card-bg': '#FFFFFF',
+  '--theme-input-bg': '#F9FAFA',
+  '--theme-text-1': '#4E5D78',
+  '--theme-text-inactive': '#B0B7C3',
+  '--theme-text-3': '#8A94A6',
+  '--theme-text-4': '#B0B7C3',
+  '--theme-bg-1': '#F9FAFA',
+  '--theme-bg-2': '#F3F3F3',
+  '--theme-bg-3': '#FAFBFC',
+  '--theme-bg-4': '#FFFFFF',
+  '--theme-border': '#F1F1F1',
+  '--theme-default-btn': '#5C0FAE'
+}
+
+const themeColorsDark = {
+  '--theme-primary-1': '#FFFFFF',
+  '--theme-primary-2': '#8A94A6',
+  '--theme-secondary-1': '#E3D1FF',
+  '--theme-primary-gradient-1': '#7028E4',
+  '--theme-primary-gradient-2': '#E5B2CA',
+  '--theme-secondary-gradient-1': '#3A537B',
+  '--theme-secondary-gradient-2': '#8F9AAC',
+  '--theme-card-bg': '#2D3751',
+  '--theme-input-bg': '#51596F',
+  '--theme-text-1': '#FFFFFF',
+  '--theme-text-inactive': '#B0B7C3', // TODO
+  '--theme-text-3': '#8A94A6', // TODO
+  '--theme-text-4': '#B0B7C3', // TODO
+  '--theme-bg-1': '#394460',
+  '--theme-bg-2': '#F3F3F3', // TODO
+  '--theme-bg-3': '#FAFBFC', // TODO
+  '--theme-bg-4': '#59617A',
+  '--theme-border': '#4E576C',
+  '--theme-default-btn': '#FFFFFF'
+}
 
 export const FrontPageGradientDiv = styled.div`
   background: ${(props) => props.theme.frontPageGradient};
@@ -162,4 +208,10 @@ export const GlobalStyles = createGlobalStyle`
   .btn-frontPage {
     color: ${(props) => props.theme.btnFrontPage};
   }
+  
+  :root {
+    ${props => {
+      const themeObject = props.theme.name === 'light' ? themeColorsLight : themeColorsDark;
+      return Object.entries(themeObject).map(x => `${x[0]}: ${x[1]}`).join(';')
+    }}
 `;
