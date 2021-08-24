06b92bbdd55882646dae18d5366e2749986ddaf3
Vijay Pushkin <mail@vijaypushkin.me>
2021-08-24 16:54:40 +0530

    Replaced redux with redux-toolkit

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,5 +1,3 @@
-import {createStore,applyMiddleware , compose , combineReducers} from 'redux';
-import thunk from 'redux-thunk';
 
 import userReducer from '../reducers/user.reducer';
 import walletReducer from '../reducers/wallet.reducer';
@@ -9,9 +7,9 @@ import pondsReducer from '../reducers/ponds.reducer';
 import poolsReducer from '../reducers/pools.reducer';
 import priceReducer from '../reducers/price.reducer';
 import homeReducer from "../reducers/home.reducers"
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+import { configureStore } from "@reduxjs/toolkit";
 
-const rootReducer = combineReducers({
+const rootReducer = {
   user : userReducer,
   wallet : walletReducer,
   swap : swapReducer,
@@ -20,9 +18,9 @@ const rootReducer = combineReducers({
   ponds : pondsReducer,
   price: priceReducer,
   home : homeReducer,
-})
+}
 
 //const store = createStore(rootReducer);
-const store = createStore(rootReducer , composeEnhancer(applyMiddleware(thunk)));
+const store = configureStore({reducer: rootReducer});
 
 export default store;
