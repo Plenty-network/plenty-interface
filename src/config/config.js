977fe34662c8a3c29f41c098b4b394af554cc932
bernd.tez <berndoostrum@gmail.com>
2021-08-26 21:50:35 +0200

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -8,7 +8,7 @@ module.exports = {
   RPC_NODES: {
     testnet: 'https://testnet.tezster.tech/',
     //mainnet: 'https://mainnet.smartpy.io/',
-    mainnet: 'https://cres2hr8uxm6.midl.dev/',
+    mainnet: 'https://mainnet-tezos.giganode.io/',
   },
   TZKT_NODES: {
     mainnet: 'https://api.tzkt.io',
