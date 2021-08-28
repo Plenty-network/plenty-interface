4d0c3a67bf3944bf2f0b55f5a865a429d1b20af9
Arhan13 <arhanchoudhury@gmail.com>
2021-08-28 17:29:48 +0530

    Added changes to node selector

--- src/Components/Header/NodeSelectorModal.js
+++ src/Components/Header/NodeSelectorModal.js
@@ -5,30 +5,75 @@ import { RPC_NODE } from "../../constants/localStorage"
 import { connect } from 'react-redux'
 import { setRPCNodeName } from '../../redux/actions/home/home.actions'
 
+const urlRegex = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gm
+
+function isValidURL(str){
+	return urlRegex.test(str)
+}
+
  function NodeSelectorModal(props) {
-	const [currentRPC, setCurrentRPC] = useState("")
-	const [customeRPC, setCustomeRPC] = useState("")
-	const LOCAL_RPC_NODES = {
+	
+	 const [currentRPC, setCurrentRPC] = useState("")
+	 const [customRPC, setCustomRPC] = useState("")
+	
+	 const LOCAL_RPC_NODES = {
 		"PLENTY": "https://cres2hr8uxm6.midl.dev/",
 		"GIGANODE": "https://mainnet-tezos.giganode.io/",
 		"CRYPTONOMIC" : "https://tezos-prod.cryptonomic-infra.tech/",
-	}
-	useEffect(() => {
-		console.log(currentRPC)
-	}, [currentRPC])
+	 }
+	 console.log(Object.entries(LOCAL_RPC_NODES))
+	 
+	 useEffect(() => {
+		 const RPCNodeInLS = localStorage.getItem(RPC_NODE)
+
+		 if (!RPCNodeInLS) {
+			setCurrentRPC("")
+			return
+		}
+		 
+		 if (!isValidURL(RPCNodeInLS)) {
+			 // set default node
+			localStorage.setItem(RPC_NODE, LOCAL_RPC_NODES['PLENTY'])
+			setCurrentRPC('PLENTY')
+			return
+		 }
+		 
+
+		const matchedNode = Object.entries(LOCAL_RPC_NODES).find(([_, nodeLink]) => {
+			if (nodeLink === RPCNodeInLS) {
+				return true
+			}
+			return false
+		 })
+
+		 if (!matchedNode) {
+			setCurrentRPC("CUSTOM")
+			setCustomRPC(RPCNodeInLS)
+			return
+		 }
+
+		 const [nodeName] = matchedNode
+		 setCurrentRPC(nodeName)
+		 // eslint-disable-next-line
+		}, [])
 
 	const setRPCInLS = () => {
 		if (currentRPC !== "CUSTOM") {
 			localStorage.setItem(RPC_NODE, LOCAL_RPC_NODES[currentRPC])
-			props.setRPCNodeName(currentRPC)
-			// window.location.reload()
+			// props.setRPCNodeName(currentRPC)
+			window.location.reload()
 		} else {
-			localStorage.setItem(RPC_NODE, customeRPC)
-			props.setRPCNodeName("CUSTOM")
-			// window.location.reload()
+			if (!isValidURL(customRPC)) {
+				// show toast
+				return
+			}
+			localStorage.setItem(RPC_NODE, customRPC)
+			// props.setRPCNodeName("CUSTOM")
+			window.location.reload()
 		}
-		props.closeNodeSelectorModal(customeRPC)
+		props.closeNodeSelectorModal(customRPC)
 	}
+	 
 	return (
 		<SimpleModal
 			title={props.title}
@@ -42,7 +87,7 @@ import { setRPCNodeName } from '../../redux/actions/home/home.actions'
 			<div className="node-selector-radio-container">
 				<ul>
 					<li onClick={() => setCurrentRPC("PLENTY")}>
-						<input type="radio" id="f-option" name="selector" />
+						<input type="radio" checked={currentRPC === 'PLENTY'} id="f-option" name="selector" />
 						<label for="f-option">Plenty node</label>
 
 						<div class="check"></div>
@@ -50,7 +95,7 @@ import { setRPCNodeName } from '../../redux/actions/home/home.actions'
 
 					<li
 						onClick={() => setCurrentRPC("GIGANODE")}>
-						<input type="radio" id="s-option" name="selector" />
+						<input type="radio" checked={currentRPC === 'GIGANODE'} id="s-option" name="selector" />
 						<label for="s-option">Giganode</label>
 
 						<div class="check">
@@ -62,23 +107,23 @@ import { setRPCNodeName } from '../../redux/actions/home/home.actions'
 						onClick={() =>
 							setCurrentRPC("CRYPTONOMIC")
 						}>
-						<input type="radio" id="t-option" name="selector" />
+						<input type="radio" id="t-option" checked={currentRPC === 'CRYPTONOMIC'} name="selector" />
 						<label for="t-option">Cryptonomic</label>
 						<div class="check">
 							<div class="inside"></div>
 						</div>
 					</li>
 					<li onClick={() => setCurrentRPC("CUSTOM")}>
-						<input type="radio" id="w-option" name="selector" />
+						<input type="radio" id="w-option" checked={currentRPC === 'CUSTOM'} name="selector" />
 						<label for="w-option">Custom</label>
 						<input
 							type="url"
 							for="w-option"
 							className="node-selector-modal-input"
 							placeholder="https://custom.tezos.node"
-							value={customeRPC}
+							value={customRPC}
 							onChange={e => {
-								setCustomeRPC(e.target.value)
+								setCustomRPC(e.target.value)
 							}}
 						/>
 						<div class="check">
