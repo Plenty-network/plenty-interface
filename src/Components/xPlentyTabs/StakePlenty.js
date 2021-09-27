c1abadc4a7502fd5cb86e3f06b9377010908e2f9
Abhishek Mishra <abhishek@tezsure.com>
2021-09-27 06:05:35 +0530

    xplenty design

---
+++ src/Components/xPlentyTabs/StakePlenty.js
@@ -0,0 +1,49 @@
+const StakePlenty = () => {
+  return (
+    <>
+      <div
+        className="swap-token-select-box bg-themed-light swap-content-box-wrapper"
+        style={{
+          borderRadius: '6px',
+          minHeight: 0,
+        }}
+      >
+        <div className="token-selector-balance-wrapper">
+          <button className="token-selector dropdown-themed">
+            <img src="" className="button-logo" />
+            <span className="span-themed">PLENTY </span>
+          </button>
+        </div>
+
+        <div className="token-user-input-wrapper">
+          <input type="text" className="token-user-input" placeholder="0.0" />
+        </div>
+      </div>
+      <div className="flex justify-between" style={{ flex: '0 0 100%' }}>
+        <p className="wallet-token-balance" style={{ cursor: 'pointer' }}>
+          Balance: 252.3142 PLENTY
+        </p>
+        <p className="wallet-token-balance">1 xPLENTY = 1.2513 PLENTY</p>
+      </div>
+      <button className="swap-content-btn xplenty-btn">Stake PLENTY</button>
+
+      <div
+        className="swap-token-select-box bg-themed-light swap-content-box-wrapper"
+        style={{
+          minHeight: 0,
+          borderRadius: '6px',
+        }}
+      >
+        <div className="token-selector-balance-wrapper">
+          <p className="xplenty-staking-apr">Staking APR</p>
+        </div>
+
+        <div className="token-user-input-wrapper">
+          <p className="xplenty-staking-apr">5.77%</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default StakePlenty;
--- src/Pages/xPlenty.js
+++ src/Pages/xPlenty.js
@@ -1,5 +1,70 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+
+import StakePlenty from '../Components/xPlentyTabs/StakePlenty';
+import UnstakePlenty from '../Components/xPlentyTabs/UnstakePlenty';
+import XplentyBalance from '../Components/xPlentyTabs/xPlentyBalance';
+
 const Stake = () => {
-  return <div>xplenty</div>;
+  return (
+    <Container fluid>
+      <Row>
+        <Col
+          sm={12}
+          md={10}
+          className="swap-content-section xplenty-content-section"
+        >
+          <div className="xplenty-content-wrapper">
+            <div className="xplenty-info-wrapper">
+              <h2 className="xplenty-heading">
+                Maximize yield by staking PLENTY for xPLENTY
+              </h2>
+              <p className="xplenty-info">
+                For every swap on Plenty, 0.09% of the swap fees are distributed
+                as PLENTY tokens to the staking pool, which are distributed
+                among the holders proportionally to the amount of their staked
+                tokens and stake duration.
+              </p>
+
+              <p className="xplenty-info">
+                When you stake PLENTY, you automatically receive xPLENTY in
+                return. The xPLENTY token is a flash loan resistant token and
+                will be used for governance. Furthermore, xPLENTY is
+                continuously compounding staking rewards and trading fees. By
+                unstaking, your xPLENTY tokens are burned and you receive all
+                originally deposited PLENTY plus all rewards collected over
+                time.
+              </p>
+            </div>
+            <div className="bg-themed swap-content-container xplenty-content-container">
+              <Tabs defaultActiveKey="stake" className="swap-container-tab">
+                <Tab eventKey="stake" title="Stake Plenty">
+                  <StakePlenty />
+                </Tab>
+
+                <Tab eventKey="unstake" title="Unstake">
+                  <UnstakePlenty />
+                </Tab>
+              </Tabs>
+              <div
+                className="swap-token-select-box bg-themed-light swap-content-box-wrapper"
+                style={{
+                  borderRadius: '6px',
+                  alignItems: 'flex-start',
+                  minHeight: 0,
+                }}
+              >
+                <XplentyBalance />
+              </div>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Stake;
