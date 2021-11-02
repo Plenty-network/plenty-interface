4cba06fd7f4c836cf4ecba97ee95ecb3feb1d222
Vijay Pushkin <mail@vijaypushkin.me>
2021-11-02 17:36:25 +0530

    removed console log

--- src/redux/slices/tokens/tokens.thunk.js
+++ src/redux/slices/tokens/tokens.thunk.js
@@ -1,13 +1,12 @@
-import { fetchTokensData } from "./tokens.api";
+import { fetchTokensData } from './tokens.api';
 
 import {
   startTokensDataFetching,
   tokensDataFetchingSuccessfull,
   tokensDataFetchingFailed,
-} from "./tokens.slice";
+} from './tokens.slice';
 
 export const tokenFetchingThunk = () => (dispatch) => {
-  console.log("Thunk Called");
   dispatch(startTokensDataFetching());
 
   fetchTokensData()
