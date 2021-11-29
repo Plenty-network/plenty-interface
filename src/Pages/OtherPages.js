f44a29c6c06213e7963513c69a0082156b47810e
Vijay Pushkin <mail@vijaypushkin.me>
2021-11-29 10:25:03 +0530

    Upgraded react router to v6

---
+++ src/Pages/OtherPages.js
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import Header from '../Components/Header/Header';
+import { useSelector } from 'react-redux';
+
+const OtherPages = (props) => {
+  const userAddress = useSelector((state) => state.wallet.address);
+  return (
+    <React.Suspense fallback={<div />}>
+      <div>
+        <Header
+          toggleTheme={props.toggleTheme}
+          theme={props.theme}
+          connecthWallet={props.connectWallet}
+          disconnectWallet={props.disconnectUserWallet}
+          walletAddress={userAddress}
+        />
+        {props.children}
+      </div>
+    </React.Suspense>
+  );
+};
+
+OtherPages.propTypes = {
+  children: PropTypes.element,
+  connectWallet: PropTypes.func.isRequired,
+  disconnectUserWallet: PropTypes.func.isRequired,
+  theme: PropTypes.string.isRequired,
+  toggleTheme: PropTypes.func.isRequired,
+  userAddress: PropTypes.func.isRequired,
+};
+
+export default OtherPages;
