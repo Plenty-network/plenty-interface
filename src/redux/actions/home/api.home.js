862e2e661870f00677396498c1e5d72f82e57994
pawan-tezsure <70758035+pawan-tezsure@users.noreply.github.com>
2021-12-03 14:03:19 +0530

    all ctez swap pairs and farms added

--- src/redux/actions/home/api.home.js
+++ src/redux/actions/home/api.home.js
@@ -472,7 +472,32 @@ export const getStorageForFarms = async (isActive, tokenPricesData) => {
           key === 'PLENTY - cTez' ||
           key === 'uUSD - YOU' ||
           key === 'uUSD - wUSDC' ||
-          key === 'uUSD - uDEFI'
+          key === 'uUSD - uDEFI' ||
+          key === 'ctez - kUSD' ||
+          key === 'ctez - USDtz' ||
+          key === 'ctez - wUSDT' ||
+          key === 'ctez - wBUSD' ||
+          key === 'ctez - wUSDC' ||
+          key === 'ctez - wDAI' ||
+          key === 'ctez - KALAM' ||
+          key === 'ctez - GIF' ||
+          key === 'ctez - ETHtz' ||
+          key === 'ctez - QUIPU' ||
+          key === 'ctez - hDAO' ||
+          key === 'ctez - kDAO' ||
+          key === 'ctez - wWETH' ||
+          key === 'ctez - uUSD' ||
+          key === 'ctez - FLAME' ||
+          key === 'ctez - SMAK' ||
+          key === 'ctez - crDAO' ||
+          key === 'ctez - PXL' ||
+          key === 'ctez - UNO' ||
+          key === 'ctez - WRAP' ||
+          key === 'ctez - wWBTC' ||
+          key === 'ctez - tzBTC' ||
+          key === 'ctez - PAUL' ||
+          key === 'ctez - INSTA' ||
+          key === 'ctez - CRUNCH'
         ) {
           dexPromises.push(
             getPriceForPlentyLpTokens(
