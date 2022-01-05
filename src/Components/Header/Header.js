69e30d8443b6288a5dff8b88148c06e1213bce0a
Ishwaryaa <ishwaryaa@tezsure.com>
2022-01-05 15:57:17 +0530

    mobile view alignment fixed

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -13,6 +13,7 @@ import Button from '../Ui/Buttons/Button';
 import HeaderBottom from './HeaderBottom';
 import useMediaQuery from '../../hooks/mediaQuery';
 import { HEADER_MODAL } from '../../constants/header';
+import Switch from '../Ui/Switch/Switch';
 
 const Header = (props) => {
   const isMobile = useMediaQuery('(max-width: 991px)');
@@ -45,7 +46,7 @@ const Header = (props) => {
       return (
         <Button
           onClick={props.connecthWallet}
-          className={clsx('px-md-3', 'connect-wallet-btn', 'button-bg')}
+          className={clsx('px-md-3', 'w-100', 'connect-wallet-btn', 'button-bg')}
         >
           <div className={clsx('connect-wallet-btn')}>
             <div className="flex flex-row align-items-center">
@@ -67,9 +68,9 @@ const Header = (props) => {
     <>
       <Container className="header" fluid>
         <Row>
-          <Col className="innerHeader" sm={12} md={12}>
+          <Col className={clsx('innerHeader')} sm={12} md={12}>
             <Navbar collapseOnSelect expand="lg" className="px-0 mx-sm-4 menu-wrapper">
-              <Navbar.Brand as={Link} to="/" className="mx-2 mx-sm-0">
+              <Navbar.Brand as={Link} to="/" className="mx-3 mx-sm-0">
                 {props.isGradientBgPage ? (
                   <LogoWhite />
                 ) : props.theme === 'light' ? (
@@ -79,7 +80,7 @@ const Header = (props) => {
                 )}
               </Navbar.Brand>
               <div className="seperator"></div>
-              <Nav.Item className="ml-auto d-lg-none align-self-lg-end">
+              <Nav.Item className="ml-auto d-none   align-self-end">
                 <a
                   className="flex header-click nav-menu-item-link px-lg-3 align-self-end align-self-lg-center"
                   onClick={props.toggleTheme}
@@ -99,7 +100,7 @@ const Header = (props) => {
                   )}
                 </a>
               </Nav.Item>
-              <Nav.Item className="ml-3 d-lg-none align-self-lg-end">
+              <Nav.Item className="ml-3 d-none  align-self-lg-end">
                 {connectWalletButton()}
               </Nav.Item>
               <Navbar.Toggle
@@ -129,11 +130,12 @@ const Header = (props) => {
               </Navbar.Toggle>
 
               <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="align-items-lg-center w-100">
+                <Nav className="align-items-lg-center w-100 mobileview">
                   <div className="col-lg-6 d-lg-flex flex-lg-row flex-column justify-content-lg-center align-items-center links">
                     <Nav.Link
                       className={clsx(
                         selectedHeader === HEADER_MODAL.TRADE ? 'menu-item-active' : 'menu-item',
+                        isExpanded && isMobile && selectedHeader !== HEADER_MODAL.TRADE && 'last',
                         'align-self-start align-self-lg-center d-lg-flex align-items-center',
                         'space-between',
                       )}
@@ -163,6 +165,7 @@ const Header = (props) => {
                     <Nav.Link
                       className={clsx(
                         selectedHeader === HEADER_MODAL.EARN ? 'menu-item-active' : 'menu-item',
+                        isExpanded && isMobile && selectedHeader !== HEADER_MODAL.EARN && 'last',
                         'align-self-end align-self-lg-center d-lg-flex align-items-center',
                       )}
                       onClick={() => setHeader(HEADER_MODAL.EARN)}
@@ -187,10 +190,10 @@ const Header = (props) => {
                         {...props}
                       />
                     )}
-
                     <Nav.Link
                       className={clsx(
                         selectedHeader === HEADER_MODAL.VOTE ? 'menu-item-active' : 'menu-item',
+                        isExpanded && isMobile && selectedHeader !== HEADER_MODAL.VOTE && 'last',
                         'align-self-end align-self-lg-center d-lg-flex align-items-center',
                       )}
                       onClick={() => setHeader(HEADER_MODAL.VOTE)}
@@ -221,6 +224,10 @@ const Header = (props) => {
                           selectedHeader === HEADER_MODAL.SETTINGS
                             ? 'menu-item-active'
                             : 'menu-item',
+                          isExpanded &&
+                            selectedHeader !== HEADER_MODAL.SETTINGS &&
+                            isMobile &&
+                            'last',
                           'align-self-end align-self-lg-center d-lg-flex align-items-center',
                           'd-lg-none',
                         )}
@@ -244,6 +251,7 @@ const Header = (props) => {
                     <Nav.Link
                       className={clsx(
                         selectedHeader === HEADER_MODAL.MORE ? 'menu-item-active' : 'menu-item',
+                        isExpanded && isMobile && selectedHeader !== HEADER_MODAL.MORE && 'last',
                         'align-self-end align-self-lg-center d-lg-flex align-items-center',
                       )}
                       onClick={() => setHeader(HEADER_MODAL.MORE)}
@@ -269,6 +277,32 @@ const Header = (props) => {
                       />
                     )}
                   </div>
+                  <Nav.Item
+                    className={clsx('mx-3 theme-mode  d-block d-lg-none ', isExpanded && 'hide')}
+                  >
+                    <div className="flex justify-between">
+                      <span className="">
+                        <span>THEME</span>
+                        <div>{props.theme === 'light' ? 'Light' : 'Dark'}</div>
+                      </span>
+                      <span className="mr-4">
+                        <Switch
+                          value={props.theme === 'light' ? true : false}
+                          onChange={props.toggleTheme}
+                          inverted={true}
+                        />
+                      </span>
+                    </div>
+                  </Nav.Item>
+                  <Nav.Item
+                    className={clsx(
+                      'mx-3',
+                      'connectwallet d-block d-lg-none align-self-center align-self-lg-end',
+                      isExpanded && 'hide',
+                    )}
+                  >
+                    {connectWalletButton()}
+                  </Nav.Item>
                   <div className="col-lg-6 d-lg-flex flex-column flex-lg-row align-items-end align-items-lg-center last">
                     <Nav.Item className="ml-auto">
                       <a
@@ -293,7 +327,7 @@ const Header = (props) => {
                       </a>
                     </Nav.Item>
 
-                    <Nav.Item className="d-none d-lg-block align-self-lg-end">
+                    <Nav.Item className="d-none d-md-block align-self-lg-end">
                       {connectWalletButton()}
                     </Nav.Item>
                     <Nav.Item>
