eb6d9fa377d210331f3ba236ba0724528b38ae92
Ishwaryaa <ishwaryaa@tezsure.com>
2022-02-14 16:50:55 +0530

    active nav link added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -138,6 +138,8 @@ const Header = (props) => {
                       className={clsx(
                         selectedHeader === HEADER_MODAL.TRADE ? 'menu-item-active' : 'menu-item',
                         (splitLocation[1] === 'swap' ||
+                          splitLocation[1] === 'stableswap' ||
+                          splitLocation[1] === 'liquidityStable' ||
                           splitLocation[1] === 'tokens' ||
                           splitLocation[1] === 'liquidity') &&
                           'selected-menu-item-active',
