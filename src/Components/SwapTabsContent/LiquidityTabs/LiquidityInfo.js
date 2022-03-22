dc79c6c1c464ab515af60a190a0f0c2f57d6adb6
Ishwaryaa <ishwaryaa@tezsure.com>
2022-03-22 16:33:06 +0530

    mobile responsivness

--- src/Components/SwapTabsContent/LiquidityTabs/LiquidityInfo.js
+++ src/Components/SwapTabsContent/LiquidityTabs/LiquidityInfo.js
@@ -6,65 +6,70 @@ import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 
 const LiquidityInfo = (props) => {
   return (
-    <div className="lq-details d-flex justify-content-between align-items-center">
+    <div className="lq-details d-flex flex-wrap justify-content-between align-items-center">
       <div>
         <img src={props.theme === 'light' ? InfoIcon : InfoIconDark} />
       </div>
-      <OverlayTrigger
-        placement="top"
-        overlay={
-          <Tooltip id="button-tooltip" {...props}>
-            {props.isStable ? props.xtztoctez : props.swapData.tokenOutPerTokenIn}
-          </Tooltip>
-        }
-      >
-        <div className="details">
-          {props.isStable ? props.xtztoctez : props.swapData.tokenOutPerTokenIn?.toFixed(3)}{' '}
-          <span className="content">
-            {props.tokenOut.name} per {props.tokenOut.name}
-          </span>
-        </div>
-      </OverlayTrigger>
-      <OverlayTrigger
-        placement="top"
-        overlay={
-          <Tooltip id="button-tooltip" {...props}>
-            {props.isStable ? props.cteztoxtz : 1 / props.swapData.tokenOutPerTokenIn}
-          </Tooltip>
-        }
-      >
-        <div className="details">
-          {props.isStable ? props.cteztoxtz : (1 / props.swapData.tokenOutPerTokenIn).toFixed(3)}{' '}
-          <span className="content">
-            {props.tokenOut.name} per {props.tokenIn.name}
-          </span>
-        </div>
-      </OverlayTrigger>
-      <OverlayTrigger
-        placement="top"
-        overlay={
-          <Tooltip id="button-tooltip" {...props}>
+      <div>
+        <OverlayTrigger
+          placement="top"
+          overlay={
+            <Tooltip id="button-tooltip" {...props}>
+              {props.isStable ? props.xtztoctez : props.swapData.tokenOutPerTokenIn}
+            </Tooltip>
+          }
+        >
+          <div className="details">
+            {props.isStable ? props.xtztoctez : props.swapData.tokenOutPerTokenIn?.toFixed(3)}{' '}
+            <span className="content">
+              {props.tokenIn.name} per {props.tokenOut.name}
+            </span>
+          </div>
+        </OverlayTrigger>
+      </div>
+      <div>
+        <OverlayTrigger
+          placement="top"
+          overlay={
+            <Tooltip id="button-tooltip" {...props}>
+              {props.isStable ? props.cteztoxtz : 1 / props.swapData.tokenOutPerTokenIn}
+            </Tooltip>
+          }
+        >
+          <div className="details">
+            {props.isStable ? props.cteztoxtz : (1 / props.swapData.tokenOutPerTokenIn).toFixed(3)}{' '}
+            <span className="content">
+              {props.tokenOut.name} per {props.tokenIn.name}
+            </span>
+          </div>
+        </OverlayTrigger>
+      </div>
+      <div>
+        <OverlayTrigger
+          placement="top"
+          overlay={
+            <Tooltip id="button-tooltip" {...props}>
+              {props.tokenIn.name === 'tez'
+                ? props.poolShare
+                : (props.lpTokenAmount.estimatedLpOutput /
+                    (props.swapData.lpTokenSupply + props.lpTokenAmount.estimatedLpOutput)) *
+                  100}
+            </Tooltip>
+          }
+        >
+          <div className="details">
+            <span className="content">Share of pool:</span>{' '}
             {props.tokenIn.name === 'tez'
               ? props.poolShare
-              : (props.lpTokenAmount.estimatedLpOutput /
-                  (props.swapData.lpTokenSupply + props.lpTokenAmount.estimatedLpOutput)) *
-                100}
-          </Tooltip>
-        }
-      >
-        <div className="details">
-          <span className="content">Share of pool:</span>{' '}
-          {props.tokenIn.name === 'tez'
-            ? props.poolShare
-            : (
-                (props.lpTokenAmount.estimatedLpOutput /
-                  (props.swapData.lpTokenSupply + props.lpTokenAmount.estimatedLpOutput)) *
-                100
-              ).toFixed(3)}{' '}
-          %
-        </div>
-      </OverlayTrigger>
-
+              : (
+                  (props.lpTokenAmount.estimatedLpOutput /
+                    (props.swapData.lpTokenSupply + props.lpTokenAmount.estimatedLpOutput)) *
+                  100
+                ).toFixed(3)}{' '}
+            %
+          </div>
+        </OverlayTrigger>
+      </div>
       <div className="details">
         {props.isStable ? '0.10' : '0.25'}% <span className="content">LP fee</span>
       </div>
