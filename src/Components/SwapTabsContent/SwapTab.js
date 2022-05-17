49c11cef68237bf52d06ee345cb2ab7a1649fc99
Ishwaryaa <ishwaryaa@tezsure.com>
2022-05-17 22:55:49 +0530

    tweaks

--- src/Components/SwapTabsContent/SwapTab.js
+++ src/Components/SwapTabsContent/SwapTab.js
@@ -687,7 +687,6 @@ const SwapTab = (props) => {
                     type="text"
                     className={clsx('token-user-input', secondTokenAmount && 'second-input-color')}
                     value={secondTokenAmount && fromExponential(secondTokenAmount)}
-                    disabled
                     placeholder="0.0"
                     disabled
                     onChange={(e) => handleSwapTokenInput(e.target.value, 'tokenOut')}
