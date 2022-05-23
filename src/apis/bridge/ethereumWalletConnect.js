f3b72af6762cb43e472877b27348d368de403429
Aniket sindhu <sindhuaniket@gmail.com>
2022-05-24 03:40:19 +0530

    Multiple wallet added + most of the edge cases covered and fixed

---
+++ src/apis/bridge/ethereumWalletConnect.js
@@ -0,0 +1,61 @@
+import Web3 from 'web3';
+import Web3Modal from 'web3modal';
+import WalletConnectProvider from '@walletconnect/web3-provider';
+import CoinbaseWalletSDK from '@coinbase/wallet-sdk';
+
+const providerOptions = {
+  walletconnect: {
+    package: WalletConnectProvider,
+    options: {
+      supportedChainIds: [1, 4, 100],
+      rpc: {
+        1: 'https://api.mycryptoapi.com/eth',
+        4: 'https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
+      },
+    },
+  },
+  coinbasewallet: {
+    package: CoinbaseWalletSDK,
+    options: {
+      appName: 'Plenty Bridge',
+      rpc: 'https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
+      chainId: 4,
+      darkMode: false,
+    },
+  },
+};
+
+const web3Modal = new Web3Modal({
+  network: 'rinkeby',
+  cacheProvider: true,
+  providerOptions,
+});
+
+export const connectWallet = async (setMetamaskAddress) => {
+  try {
+    const provider = await web3Modal.connect();
+    console.log('provider', provider);
+    const web3 = new Web3(provider);
+    const address = await web3.eth.getAccounts();
+    if (setMetamaskAddress) {
+      setMetamaskAddress(address[0]);
+      localStorage.setItem('isWalletConnected', true);
+    }
+    console.log('web3', web3);
+    console.log('web3.eth.accounts', address[0]);
+    return {
+      address: address[0],
+      web3,
+      provider,
+    };
+  } catch (e) {
+    console.log('Error', e);
+    if (setMetamaskAddress) {
+      localStorage.setItem('isWalletConnected', false);
+    }
+  }
+};
+
+export const disconnectWallet = async () => {
+  web3Modal.clearCachedProvider();
+};
