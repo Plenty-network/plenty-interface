829b4a2aa33d0d5ccd215b1ee154bdac8f9fae26
Your Name <ashutosh@tezsure.com>
2022-07-08 12:49:11 +0530

    governance container added

--- src/Pages/Governance/Governance.js
+++ src/Pages/Governance/Governance.js
@@ -78,7 +78,7 @@ const Governance = (props) => {
 
   return (
     <>
-      <Container className={` ${styles.govContainerr}`} fluid>
+      <Container className={` ${styles.govContainer3}`} fluid>
         <Row className={clsx('row justify-content-center', !isMobile && styles.govContainerInner)}>
           <Col xs={20} sm={8} md={10} lg={6} xl={6}>
             <VoteText
