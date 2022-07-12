dc629e0f960089536d4df7b653ab9fe2ed8628ba
Udit-Kapoor <udit.dude01@gmail.com>
2022-07-12 20:14:03 +0530

    Uncommenting testing consoles

--- src/redux/slices/farms/farms.api.js
+++ src/redux/slices/farms/farms.api.js
@@ -300,9 +300,6 @@ const fetchStorageForDualStakingContract = async (
   
     const totalLiquidty = totalSupply * priceOfStakeTokenInUsd;
 
-    if (identifier === 'EURL - agEUR.e')
-    console.log(totalLiquidty  , totalSupply  , priceOfStakeTokenInUsd);
-
     return {
       success: true,
       identifier,
@@ -1236,7 +1233,6 @@ const getPriceForPlentyLpTokens = async (
       const token2Pool = parseInt(storageResponse.data.args[3].int);
       const lpTokenTotalSupply = parseInt(storageResponse.data.args[0].args[0].args[2].int); 
       
-      console.log(token1Pool , token2Pool , lpTokenTotalSupply);
       const tokenData = {};
 
       let idx;
@@ -1261,26 +1257,19 @@ const getPriceForPlentyLpTokens = async (
         tokenDecimal: 18,
       };
 
-      console.log(tokenData);
-
       let token1Amount = (Math.pow(10, lpTokenDecimal) * token1Pool) / lpTokenTotalSupply;
 
-      console.log('t1 ' + token1Amount);
       token1Amount =
         (token1Amount * tokenData['token1'].tokenValue) /
         Math.pow(10, tokenData['token1'].tokenDecimal);
 
-      console.log('t1VAL ' + token1Amount);
-
       let token2Amount = (Math.pow(10, lpTokenDecimal) * token2Pool) / lpTokenTotalSupply;
-      console.log('t2 ' + token2Amount);
+
       token2Amount =
         (token2Amount * tokenData['token2'].tokenValue) /
         Math.pow(10, tokenData['token2'].tokenDecimal);
-        console.log('t2VAL ' + token2Amount);
 
       const totalAmount = (token1Amount + token2Amount).toFixed(2);
-      console.log(totalAmount);
       return {
         success: true,
         identifier,
